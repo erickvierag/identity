8b8f761ed7395131fe5d8482504f61106de558d3
erick <eviera@dotnet.com.mx>
2017-05-29 09:14:52 -0500

    Agregar archivos de proyecto.

---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var TokenCLient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var TokenResponse = await TokenCLient.RequestResourceOwnerPasswordAsync("erick","password", "api1");
+
+            if (TokenResponse.IsError)
+            {
+                Console.WriteLine(TokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(TokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(TokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        
+    }
+    }
+}
